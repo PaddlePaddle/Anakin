509dc64975d60a91d232369311a4a15e1fc3f66d
lixiaoyang05 <lixiaoyang05@baidu.com>
2018-06-05 21:03:08 +0800

    fix error while building arm

--- framework/operators/axpy.cpp
+++ framework/operators/axpy.cpp
@@ -3,7 +3,7 @@
 namespace anakin {
 
 namespace ops {
-
+#ifdef USE_CUDA
 template<>
 void Axpy<NV, AK_FLOAT, Precision::FP32>::operator()(
     OpContext<NV>& ctx,
@@ -14,9 +14,20 @@ void Axpy<NV, AK_FLOAT, Precision::FP32>::operator()(
     auto& param = impl->_param_axpy;
     impl->_funcs_axpy(ins, outs, param, ctx);
 }
-
+#endif
 /// TODO ... specialization other type of operator
-
+#ifdef USE_ARM_PLACE
+template<>
+void Axpy<ARM, AK_FLOAT, Precision::FP32>::operator()(
+    OpContext<ARM>& ctx,
+    const std::vector<Tensor4dPtr<ARM, AK_FLOAT> >& ins,
+    std::vector<Tensor4dPtr<ARM, AK_FLOAT> >& outs) {
+    auto* impl =
+        static_cast<AxpyHelper<ARM, AK_FLOAT, Precision::FP32>*>(this->_helper);
+    auto& param = impl->_param_axpy;
+    impl->_funcs_axpy(ins, outs, param, ctx);
+}
+#endif
 
 /// set helper
 template<typename Ttype, DataType Dtype, Precision Ptype>
@@ -55,9 +66,15 @@ template class AxpyHelper<NV, AK_FLOAT, Precision::INT8>;
 #endif
 
 #ifdef USE_ARM_PLACE
+#ifdef ANAKIN_TYPE_FP32
 template class AxpyHelper<ARM, AK_FLOAT, Precision::FP32>;
+#endif
+#ifdef ANAKIN_TYPE_FP16
 template class AxpyHelper<ARM, AK_FLOAT, Precision::FP16>;
+#endif
+#ifdef ANAKIN_TYPE_INT8
 template class AxpyHelper<ARM, AK_FLOAT, Precision::INT8>;
+#endif
 #endif
 
 // register helper
