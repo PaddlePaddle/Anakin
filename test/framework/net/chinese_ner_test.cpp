1e5875e04391064e1794134e6629297877d078e3
shixiaowei02 <shixiaowei02@baidu.com>
2018-06-15 14:42:38 +0800

    support mainbody

--- test/framework/net/chinese_ner_test.cpp
+++ test/framework/net/chinese_ner_test.cpp
@@ -17,7 +17,6 @@
 DEFINE_GLOBAL(std::string, model_dir, "");
 DEFINE_GLOBAL(std::string, input_file, "");
 
-
 //#define WITH_MENTION
 
 void getModels(std::string path, std::vector<std::string>& files) {
@@ -116,7 +115,6 @@ int get_batch_data_offset(
 }
 
 #ifdef USE_X86_PLACE
-
 TEST(NetTest, chinese_ner_executor) {
     std::vector<std::string> models;
     getModels(GLB_model_dir, models);
@@ -193,7 +191,6 @@ TEST(NetTest, chinese_ner_executor) {
     }
     LOG(INFO)<<"elapse time: "<<timer.get_average_ms()<<" ms";
 }
-
 #endif
 
 int main(int argc, const char** argv) {
