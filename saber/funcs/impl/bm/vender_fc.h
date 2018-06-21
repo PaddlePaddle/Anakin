b1aa39dcfc107d06fa1f150cfcda30ffbbe40bdf
tong.liu <tong.liu@bitmain.com>
2018-06-21 15:38:08 +0800

    modify activation op and test

--- saber/funcs/impl/bm/vender_fc.h
+++ saber/funcs/impl/bm/vender_fc.h
@@ -1,20 +1,5 @@
-/* Copyright (c) 2018 Baidu, Inc. All Rights Reserved.
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-   
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License. 
-*/
-
-#ifndef ANAKIN_SABER_FUNCS_CUDA_CUBLAS_FC_H
-#define ANAKIN_SABER_FUNCS_CUDA_CUBLAS_FC_H
+#ifndef ANAKIN_SABER_FUNCS_BMDNN_FC_H
+#define ANAKIN_SABER_FUNCS_BMDNN_FC_H
 
 #include "saber/funcs/impl/impl_fc.h"
 
@@ -43,23 +28,12 @@ public:
     typedef typename DataTensor_out::Dtype OutDataType;
     typedef typename OpTensor::Dtype OpDataType;
 
-    VenderFc() = default;
-    ~VenderFc() {
-        if (_handle != nullptr) {
-            CUBLAS_CHECK(cublasDestroy(_handle));
-        }
-    }
+    VenderFc() {};
+    ~VenderFc() {}
 
     virtual SaberStatus init(const std::vector<DataTensor_in *>& inputs,
                             std::vector<DataTensor_out *>& outputs,
                             FcParam<OpTensor>& param, Context<BM>& ctx){
-        // get context
-        this->_ctx = ctx;
-        cudaStream_t cuda_stream;
-        cuda_stream = ctx.get_compute_stream();
-
-        CUBLAS_CHECK(cublasCreate(&_handle));
-        CUBLAS_CHECK(cublasSetStream(_handle, cuda_stream));
         return create(inputs, outputs, param, ctx);
     }
 
@@ -94,16 +68,10 @@ public:
 
     virtual SaberStatus dispatch(const std::vector<DataTensor_in *>& inputs,
                             std::vector<DataTensor_out *>& outputs,
-                            FcParam<OpTensor>& param);
+                            FcParam<OpTensor>& param){
 
+    };
 
-private:
-    bool _flag_trans_weights{false};
-    int _M;
-    int _K;
-    int _N;
-    cublasHandle_t _handle;
-    bool _is_continue_buf{true};
 };
 
 template class VenderFc<BM, AK_FLOAT, AK_FLOAT, AK_FLOAT, NCHW, NCHW, NCHW>;
@@ -111,4 +79,4 @@ template class VenderFc<BM, AK_FLOAT, AK_FLOAT, AK_FLOAT, NCHW, NCHW, NCHW>;
 
 } //namespace anakin
 
-#endif //ANAKIN_SABER_FUNCS_CUDA_CUBLAS_FC_H
+#endif // ANAKIN_SABER_FUNCS_CUDA_CUBLAS_FC_H
