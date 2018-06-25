e318008f7367822a7b4c6771aff457f63ba3d454
frank.xie <frank.xie@bitmain.com>
2018-06-25 11:46:19 +0800

    Update buffer test for BM

--- test/saber/bm/test_saber_buffer_BM.cpp
+++ test/saber/bm/test_saber_buffer_BM.cpp
@@ -4,12 +4,22 @@
 
 using namespace anakin::saber;
 
-template <DataType datatype>
+static bm_handle_t handle;
+
+int get_bm_size() {
+    return 1;
+}
+
+template <DataType Ddatatype, DataType Hdatatype>
 void test_buffer() {
+
+    //TODO: init in another place
+    bmdnn_init(&handle);
 
     typedef TargetWrapper<X86> X86_API;
     typedef TargetWrapper<BM> BM_API;
-    typedef typename DataTrait<datatype>::dtype Dtype;
+    typedef typename DataTrait<Ddatatype>::dtype Ddtype;
+    typedef typename DataTrait<Hdatatype>::dtype Hdtype;
     typedef Buffer<X86> BufferH;
     typedef Buffer<BM> BufferD;
 
@@ -17,30 +27,30 @@ void test_buffer() {
     int n1 = 2048;
 
     void* tmp_x86;
-    Dtype* x86_ptr;
-    X86_API::mem_alloc(&tmp_x86, sizeof(Dtype) * n0);
-    x86_ptr = static_cast<Dtype*>(tmp_x86);
+    Hdtype* x86_ptr;
+    X86_API::mem_alloc(&tmp_x86, sizeof(Hdtype) * n0);
+    x86_ptr = static_cast<Hdtype*>(tmp_x86);
 
     for (int i = 0; i < n0; i++) {
-        x86_ptr[i] = static_cast<Dtype>(i);
+        x86_ptr[i] = static_cast<Hdtype>(i);
     }
 
     void* tmp_bm;
-    Dtype* bm_ptr;
-    BM_API::mem_alloc(&tmp_bm, sizeof(Dtype) * n0);
-    bm_ptr = static_cast<Dtype*>(tmp_bm);
+    Ddtype* bm_ptr;
+    BM_API::mem_alloc(&tmp_bm, get_bm_size() * n0);
+    bm_ptr = static_cast<Ddtype*>(tmp_bm);
 
     LOG(INFO) << "Buffer: test default(empty) constructor";
     BufferH x86_buf0;
     BufferD bm_buf0;
 
     LOG(INFO) << "Buffer: test constructor with data size";
-    BufferH x86_buf1(n0 * sizeof(Dtype));
-    BufferD bm_buf1(n0 * sizeof(Dtype));
+    BufferH x86_buf1(n0 * sizeof(Hdtype));
+    BufferD bm_buf1(n0 * sizeof(Ddtype));
 
     LOG(INFO) << "Buffer: test constructor with data pointer, size and device id";
-    BufferH x86_buf2(x86_ptr, n0 * sizeof(Dtype), X86_API::get_device_id());
-    BufferD bm_buf2(bm_ptr, n0 * sizeof(Dtype), BM_API::get_device_id());
+    BufferH x86_buf2(x86_ptr, n0 * sizeof(Hdtype), X86_API::get_device_id());
+    BufferD bm_buf2(bm_ptr, n0 * get_bm_size(), BM_API::get_device_id());
 
     LOG(INFO) << "Buffer: test copy constructor";
     BufferH x86_buf3(x86_buf2);
@@ -62,18 +72,18 @@ void test_buffer() {
             "shared buffer should have same data count";
 
     LOG(INFO) << "Buffer: test re_alloc";
-    x86_buf1.re_alloc(n1 * sizeof(Dtype));
-    bm_buf1.re_alloc(n1 * sizeof(Dtype));
-    CHECK_EQ(x86_buf1.get_count(), n1 * sizeof(Dtype)) << "buffer count error";
-    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Dtype)) << "buffer capacity error";
-    CHECK_EQ(bm_buf1.get_count(), n1 * sizeof(Dtype)) << "buffer count error";
-    CHECK_EQ(bm_buf1.get_capacity(), n1 * sizeof(Dtype)) << "buffer capacity error";
-    x86_buf1.re_alloc(n0 * sizeof(Dtype));
-    bm_buf1.re_alloc(n0 * sizeof(Dtype));
-    CHECK_EQ(x86_buf1.get_count(), n0 * sizeof(Dtype)) << "buffer count error";
-    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Dtype)) << "buffer capacity error";
-    CHECK_EQ(x86_buf1.get_count(), n0 * sizeof(Dtype)) << "buffer count error";
-    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Dtype)) << "buffer capacity error";
+    x86_buf1.re_alloc(n1 * sizeof(Hdtype));
+    bm_buf1.re_alloc(n1 * sizeof(Ddtype));
+    CHECK_EQ(x86_buf1.get_count(), n1 * sizeof(Hdtype)) << "buffer count error";
+    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Hdtype)) << "buffer capacity error";
+    CHECK_EQ(bm_buf1.get_count(), n1 * sizeof(Ddtype)) << "buffer count error";
+    CHECK_EQ(bm_buf1.get_capacity(), n1 * sizeof(Ddtype)) << "buffer capacity error";
+    x86_buf1.re_alloc(n0 * sizeof(Hdtype));
+    bm_buf1.re_alloc(n0 * sizeof(Ddtype));
+    CHECK_EQ(x86_buf1.get_count(), n0 * sizeof(Hdtype)) << "buffer count error";
+    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Hdtype)) << "buffer capacity error";
+    CHECK_EQ(x86_buf1.get_count(), n0 * sizeof(Hdtype)) << "buffer count error";
+    CHECK_EQ(x86_buf1.get_capacity(), n1 * sizeof(Hdtype)) << "buffer capacity error";
 
     LOG(INFO) << "Buffer: test get_id()";
     LOG(INFO) << "X86 device id: " << x86_buf0.get_id() << \
@@ -84,8 +94,8 @@ void test_buffer() {
     LOG(INFO) << "Buffer: test deep_cpy()";
     x86_buf1.sync_copy_from(x86_buf2);
     LOG(INFO) << "deep copy between two host buffer: ";
-    const Dtype* ptr1 = static_cast<const Dtype*>(x86_buf1.get_data());
-    const Dtype* ptr2 = static_cast<const Dtype*>(x86_buf1.get_data());
+    const Hdtype* ptr1 = static_cast<const Hdtype*>(x86_buf1.get_data());
+    const Hdtype* ptr2 = static_cast<const Hdtype*>(x86_buf1.get_data());
 
     for (int i = 0; i < 10; i++) {
         std::cout << ptr1[i] << std::endl;
@@ -96,7 +106,7 @@ void test_buffer() {
     bm_buf1.sync_copy_from(x86_buf2);
     x86_buf1.sync_copy_from(bm_buf1);
     LOG(INFO) << "deep copy from device buffer to host buffer: ";
-    ptr1 = static_cast<const Dtype*>(x86_buf1.get_data());
+    ptr1 = static_cast<const Hdtype*>(x86_buf1.get_data());
 
     for (int i = 0; i < 10; i++) {
         std::cout << ptr1[i] << std::endl;
@@ -104,7 +114,7 @@ void test_buffer() {
 }
 
 TEST(TestSaberBufferBM, test_buffer_memcpy) {
-    test_buffer<AK_BM>();
+    test_buffer<AK_BM, AK_FLOAT>();
 }
 
 int main(int argc, const char** argv) {
