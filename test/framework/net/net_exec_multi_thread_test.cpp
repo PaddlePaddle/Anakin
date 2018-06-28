bff601c294d62502ee92754df621a2f557c2760f
guangzhixie <guangzhi.xie@berkeley.edu>
2018-06-28 09:43:19 +0800

    Add back missing files

---
+++ test/framework/net/net_exec_multi_thread_test.cpp
@@ -0,0 +1,149 @@
+#include <string>
+#include "net_test.h"
+#include "saber/funcs/timer.h"
+#include <chrono>
+
+std::string model_path = "/home/chaowen/anakin_v2/model_v2/anakin-models/adu/anakin_models/diepsie_light_head/yolo_lane_v2.anakin.bin";
+
+#ifdef USE_CUDA
+#if 1
+TEST(NetTest, nv_net_execute_muti_thread_sync_test) {
+#if 1 // use host input
+    //Env<NV>::env_init(1);
+    LOG(WARNING) << "Sync Runing multi_threads for model: " << model_path;
+    Worker<NV, AK_FLOAT, Precision::FP32>  workers(model_path, 10); 
+    workers.register_inputs({"input_0"});
+    workers.register_outputs({"softmax_out"});    
+    workers.Reshape("input_0", {1, 384, 960, 3});
+
+    workers.launch();
+
+    std::vector<Tensor4dPtr<target_host<NV>::type, AK_FLOAT> > host_tensor_p_in_list;
+    // get in
+    saber::Shape valid_shape_in({1, 384, 960, 3});
+    Tensor4dPtr<target_host<NV>::type, AK_FLOAT> h_tensor_in = new Tensor4d<target_host<NV>::type, AK_FLOAT>(valid_shape_in);
+    float* h_data = h_tensor_in->mutable_data();
+    for (int i=0; i<h_tensor_in->size(); i++) {
+        h_data[i] = 1.0f;
+    }
+    host_tensor_p_in_list.push_back(h_tensor_in);
+
+    int epoch = 1000;
+
+    // Running 
+    for(int i=0; i<epoch; i++) {
+        auto  d_tensor_p_out_list = workers.sync_prediction(host_tensor_p_in_list);
+
+        // get the output
+        auto d_tensor_p = d_tensor_p_out_list[0];
+    }
+
+    // get exec times
+#ifdef ENABLE_OP_TIMER
+    auto& times_map = workers.get_task_exe_times_map_of_sync_api();
+    for (auto it = times_map.begin(); it!=times_map.end(); it++) {
+        LOG(WARNING) << " threadId: " << it->first << " processing " << it->second.size() << " tasks";
+        for (auto time_in_ms : it->second) { 
+            LOG(INFO) << "      \\__task avg time: " << time_in_ms;
+        }
+    }
+#endif
+
+#endif
+
+#if 0 // use device input
+    Env<NV>::env_init(1);
+    LOG(WARNING) << "Sync Runing multi_threads for model: " << model_path;
+    Worker<NV, AK_FLOAT, Precision::FP32>  workers(model_path, 1); 
+    workers.register_inputs({"input_0"});
+    workers.register_outputs({"softmax_out"});    
+    workers.Reshape("input_0", {1, 384, 960, 3});
+
+    workers.launch();
+
+    std::vector<Tensor4dPtr<target_host<NV>::type, AK_FLOAT> > host_tensor_p_in_list;
+    // get in
+    saber::Shape valid_shape_in({1, 384, 960, 3});
+    Tensor4dPtr<target_host<NV>::type, AK_FLOAT> h_tensor_in = new Tensor4d<target_host<NV>::type, AK_FLOAT>(valid_shape_in);
+    float* h_data = h_tensor_in->mutable_data();
+    for (int i=0; i<h_tensor_in->size(); i++) {
+        h_data[i] = 1.0f;
+    }
+    host_tensor_p_in_list.push_back(h_tensor_in);
+
+    std::vector<Tensor4dPtr<NV, AK_FLOAT> > device_tensor_p_in_list;
+    for (int i=0; i<host_tensor_p_in_list.size(); i++) {
+        Tensor4dPtr<NV, AK_FLOAT> d_tensor_in = new Tensor4d<NV, AK_FLOAT>(host_tensor_p_in_list[i]->valid_shape());
+        d_tensor_in->copy_from(*(host_tensor_p_in_list[i]));
+        device_tensor_p_in_list.push_back(d_tensor_in);
+    }
+
+    int epoch = 10;
+
+    // Running 
+    for (int i=0; i<epoch; i++) {
+        Context<NV> ctx(0, 0, 0);
+        saber::SaberTimer<NV> my_time;
+
+        my_time.start(ctx);
+        auto  d_tensor_p_out_list = workers.sync_prediction_device(device_tensor_p_in_list);
+        my_time.end(ctx);
+        LOG(INFO)<<"muti thread single task exec time: "<<my_time.get_average_ms()/epoch << " ms";
+
+        // get the output
+        auto d_tensor_p = d_tensor_p_out_list[0];
+    }
+#endif
+
+}
+#endif
+
+#if 0
+TEST(NetTest, net_execute_muti_thread_async_test) {
+    LOG(WARNING) << "Async Runing multi_threads for model: " << model_path;
+    Worker<NV, AK_FLOAT, Precision::FP32>  workers(model_path, 10); 
+    workers.register_inputs({"input_0"});
+    workers.register_outputs({"softmax_out"});    
+    workers.Reshape("input_0", {1, 384, 960, 3});
+
+    workers.launch();
+
+    std::vector<Tensor4dPtr<target_host<NV>::type, AK_FLOAT> > host_tensor_p_in_list;
+    // get in
+    saber::Shape valid_shape_in({1, 384, 960, 3});
+    Tensor4dPtr<target_host<NV>::type, AK_FLOAT> h_tensor_in = new Tensor4d<target_host<NV>::type, AK_FLOAT>(valid_shape_in);
+    float* h_data = h_tensor_in->mutable_data();
+    for (int i=0; i<h_tensor_in->size(); i++) {
+        h_data[i] = 1.0f;
+    }
+    host_tensor_p_in_list.push_back(h_tensor_in);
+
+    int epoch = 10000;
+
+    // Running 
+    for(int i=0; i<epoch; i++) {
+        workers.async_prediction(host_tensor_p_in_list);
+    }
+
+    // get the output
+    int iterator = epoch;
+    while(iterator) {
+        if(!workers.empty()) {
+            auto d_tensor_p = workers.async_get_result()[0];
+            // get hte data of d_tensor_p
+            
+            iterator--;
+        }
+    }
+
+}
+#endif 
+#endif
+
+int main(int argc, const char** argv){
+    // initial logger
+    logger::init(argv[0]);
+	InitTest();
+	RUN_ALL_TESTS(argv[0]);	
+	return 0;
+}
---
+++ test/framework/net/net_test.h
@@ -0,0 +1,98 @@
+/* Copyright (c) 2018 Baidu, Inc. All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+   
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License. 
+*/
+
+#ifndef ANAKIN_NET_TEST_H
+#define ANAKIN_NET_TEST_H
+
+#include <iostream>
+#include "utils/unit_test/aktest.h"
+#include "utils/logger/logger.h"
+#include "graph_base.h"
+#include "graph.h"
+#include "scheduler.h"
+#include "net.h"
+#include "worker.h"
+
+using namespace anakin;
+using ::anakin::test::Test;
+
+using namespace anakin::graph;
+
+/**
+ * \brief Graph test is base Test class for anakin graph funciton.  
+ */
+class NetTest: public Test {
+public:
+    NetTest(){}
+
+    void SetUp(){}
+
+    void TearDown(){}
+
+protected:
+};
+
+#ifdef USE_CUDA
+void test_print(Tensor4dPtr<NV, AK_FLOAT>& out_tensor_p) {
+    Tensor4d<target_host<NV>::type, AK_FLOAT> h_tensor_result;
+    h_tensor_result.re_alloc(out_tensor_p->valid_shape());
+    LOG(ERROR) << "result count : " << h_tensor_result.valid_shape().count();
+    h_tensor_result.copy_from(*out_tensor_p);
+    for (int i = 0; i < h_tensor_result.valid_size(); i++) {
+        LOG(INFO) << " GET OUT (" << i << ") " << h_tensor_result.mutable_data()[i];
+    }
+}
+#endif
+
+template<typename Ttype, DataType Dtype>
+double tensor_average(Tensor4dPtr<Ttype, Dtype>& out_tensor_p) {
+    double sum = 0.0f;
+#ifdef USE_CUDA
+    float* h_data = new float[out_tensor_p->valid_size()];
+    const float* d_data = out_tensor_p->data();
+    CUDA_CHECK(cudaMemcpy(h_data, d_data, out_tensor_p->valid_size()*sizeof(float), cudaMemcpyDeviceToHost));
+#else
+	float* h_data = out_tensor_p->data();
+#endif
+    for (int i=0; i<out_tensor_p->valid_size(); i++) {
+		sum+=h_data[i];
+    }
+    return sum/out_tensor_p->valid_size();
+}
+
+
+#ifdef USE_X86_PLACE
+static int record_dev_tensorfile(const Tensor4d<X86, AK_FLOAT>* dev_tensor, const char* locate) {
+    Tensor<target_host<X86>::type, AK_FLOAT, NCHW> host_temp;
+    host_temp.re_alloc(dev_tensor->valid_shape());
+    host_temp.copy_from(*dev_tensor);
+    FILE* fp = fopen(locate, "w+");
+    int size = host_temp.valid_shape().count();
+    if (fp == 0) {
+        LOG(ERROR) << "[ FAILED ] file open target txt: " << locate;
+    } else {
+        for (int i = 0; i < size; ++i) {
+            fprintf(fp, "%.18f \n", i, (host_temp.data()[i]));
+        }
+        fclose(fp);
+    }
+    LOG(INFO) << "[ SUCCESS ] Write " << size << " data to: " << locate;
+    return 0;
+}
+#endif
+
+#endif
+
+
