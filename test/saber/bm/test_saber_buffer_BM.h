62a04c8f1994447bfded4b9d2e3e03db7fb07b6d
guangzhixie <guangzhi.xie@berkeley.edu>
2018-06-28 09:42:35 +0800

    Add back missing files

---
+++ test/saber/bm/test_saber_buffer_BM.h
@@ -0,0 +1,20 @@
+#ifndef ANAKIN_TEST_SABER_TEST_SABER_BUFFER_BM_H
+#define ANAKIN_TEST_SABER_TEST_SABER_BUFFER_BM_H
+
+#include "utils/unit_test/aktest.h"
+#include "utils/logger/logger.h"
+
+using namespace anakin::test;
+
+class TestSaberBufferBM : public Test {
+public:
+    TestSaberBufferBM() {}
+    ~TestSaberBufferBM() {}
+
+protected:
+    virtual void setup() {}
+    virtual void teardown() {}
+
+};
+
+#endif //ANAKIN_TEST_SABER_TEST_SABER_BUFFER_BM_H
