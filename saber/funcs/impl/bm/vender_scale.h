e7d2d043abd4d5a0d8d9feef387f275b1c0a0ea2
guangzhixie <guangzhi.xie@berkeley.edu>
2018-06-29 13:52:16 +0800

    cleanup

--- saber/funcs/impl/bm/vender_scale.h
+++ saber/funcs/impl/bm/vender_scale.h
@@ -81,16 +81,14 @@ public:
         if (param.bias_term) {
             float* host_bias = &param.scale_b[0];
             float* host_extension = new float[size];
-            printf(".........\n");
+            //printf(".........\n");
             int dim = inner_dim * scale_dim;
-            //host_bias[0] = 1;
-            //host_bias[1] = 2;
             for (int i = 0; i < size; ++i) {
                  int bias_dim = (i % dim) / inner_dim;
                  host_extension[i] = host_bias[bias_dim];
-                 printf("%f, ", host_extension[i]);
+                 //printf("%f, ", host_extension[i]);
             }
-            printf("\n");
+            //printf("\n");
 
             BMDNN_CHECK(bmdnn_bias_forward(_handle, out_data, bm_mem_from_system(host_extension),
                     outer_dim, scale_dim * inner_dim, out_data));
