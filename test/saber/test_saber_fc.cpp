1776b6b3748a73a8919808cf76e8e1b0d0d932b5
luoxiaocheng <luoxiaocheng@baidu.com>
2018-07-31 15:28:22 +0800

    add test_saber-fc.cpp

---
+++ test/saber/test_saber_fc.cpp
@@ -0,0 +1,8 @@
+#include "saber/core/context.h"
+#include "saber/funcs/fc.h"
+#include "saber/core/tensor_op.h"
+#include "saber/saber_types.h"
+#include "test_saber_func.h"
+#include <vector>
+#include <ctime>
+
