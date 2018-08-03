9b01c823fbc07aa1ccc082a795050b2800523128
lixiaoyang05 <lixiaoyang05@baidu.com>
2018-08-03 10:21:06 +0800

    fix error in activation when input size <= 32

--- saber/lite/funcs/neon/saber_activation.cpp
+++ saber/lite/funcs/neon/saber_activation.cpp
@@ -105,33 +105,35 @@ SaberStatus SaberActivation::dispatch(const std::vector<Tensor<CPU, AK_FLOAT> *>
                 }
                 
 #else
-                asm volatile (
-                        "1:                                     @ loop header\n"
-                        "vld1.32  {d0-d1}, [%[din]]!            @ load din 0\n"
-                        "vld1.32  {d2-d3}, [%[din]]!            @ load din 0\n"
-                        "vld1.32  {d4-d5}, [%[din]]!            @ load din 0\n"
-                        "vld1.32  {d6-d7}, [%[din]]!            @ load din 0\n"
+                if (cnt > 0) {
+                    asm volatile (
+                    "1:                                     @ loop header\n"
+                            "vld1.32  {d0-d1}, [%[din]]!            @ load din 0\n"
+                            "vld1.32  {d2-d3}, [%[din]]!            @ load din 0\n"
+                            "vld1.32  {d4-d5}, [%[din]]!            @ load din 0\n"
+                            "vld1.32  {d6-d7}, [%[din]]!            @ load din 0\n"
 
-                        "vmax.f32 q8, q0, %q[vzero]             @ relu\n"
-                        "vmax.f32 q9, q1, %q[vzero]             @ relu\n"
-                        "vmax.f32 q10, q2, %q[vzero]            @ relu\n"
-                        "vmax.f32 q11, q3, %q[vzero]            @ relu\n"
+                            "vmax.f32 q8, q0, %q[vzero]             @ relu\n"
+                            "vmax.f32 q9, q1, %q[vzero]             @ relu\n"
+                            "vmax.f32 q10, q2, %q[vzero]            @ relu\n"
+                            "vmax.f32 q11, q3, %q[vzero]            @ relu\n"
 
-                        "vst1.32  {d16-d17}, [%[dout]]!         @ store result, add pointer\n"
-                        "pld [%[din]]                           @ preload data\n"
-                        "vst1.32  {d18-d19}, [%[dout]]!         @ store result, add pointer\n"
-                        "pld [%[din], #128]                     @ preload data\n"
-                        "vst1.32  {d20-d21}, [%[dout]]!         @ store result, add pointer\n"
-                        "pld [%[din], #256]                     @ preload data\n"
-                        "vst1.32  {d22-d23}, [%[dout]]!         @ store result, add pointer\n"
-                        "pld [%[din], #384]                     @ preload data\n"
+                            "vst1.32  {d16-d17}, [%[dout]]!         @ store result, add pointer\n"
+                            "pld [%[din]]                           @ preload data\n"
+                            "vst1.32  {d18-d19}, [%[dout]]!         @ store result, add pointer\n"
+                            "pld [%[din], #128]                     @ preload data\n"
+                            "vst1.32  {d20-d21}, [%[dout]]!         @ store result, add pointer\n"
+                            "pld [%[din], #256]                     @ preload data\n"
+                            "vst1.32  {d22-d23}, [%[dout]]!         @ store result, add pointer\n"
+                            "pld [%[din], #384]                     @ preload data\n"
 
-                        "subs %[cnt], #1                        @ loop count minus 1\n"
-                        "bne    1b                              @ jump to main loop start point\n"
-                :[dout] "+r"(ptr_out_thread), [din] "+r"(ptr_in_thread), [cnt] "+r"(cnt)
-                :[vzero] "w" (vzero)
-                :"q0", "q1", "q2", "q3", "q8", "q9", "q10", "q11"
-                );
+                            "subs %[cnt], #1                        @ loop count minus 1\n"
+                            "bne    1b                              @ jump to main loop start point\n"
+                    :[dout] "+r"(ptr_out_thread), [din] "+r"(ptr_in_thread), [cnt] "+r"(cnt)
+                    :[vzero] "w" (vzero)
+                    :"q0", "q1", "q2", "q3", "q8", "q9", "q10", "q11"
+                    );
+                }
 #endif
                 for (int j = 0; j < neon_loop_remain; ++j) {
                     ptr_out_thread[0] = ptr_in_thread[0] > 0.f? ptr_in_thread[0] : 0.f;
