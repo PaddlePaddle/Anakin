ac62a9f25735357bcbd9ce42a9511613e1b5a43b
mengkai94 <534916442@qq.com>
2018-08-06 14:56:45 +0800

    Update saber_crop.cpp

--- saber/funcs/impl/x86/saber_crop.cpp
+++ saber/funcs/impl/x86/saber_crop.cpp
@@ -3,7 +3,8 @@
 namespace anakin{
 namespace saber {
 
-SaberStatus SaberCrop<X86, AK_FLOAT>::dispatch(
+template <DataType OpDtype>
+SaberStatus SaberCrop<X86, OpDtype>::dispatch(
         const std::vector<Tensor<X86>*>& inputs,
         std::vector<Tensor<X86>*>& outputs,
         CropParam<X86>& param) {
@@ -12,8 +13,8 @@ SaberStatus SaberCrop<X86, AK_FLOAT>::dispatch(
     int in_h = inputs[0]->height();
     int in_w = inputs[0]->width();
     int offset=0;
-    const float* ptr_in = (const float*)inputs[0]->data();
-    float* ptr_out = (float*)outputs[0]->mutable_data();
+    const OpDtype* ptr_in = (const OpDtype*)inputs[0]->data();
+    OpDtype* ptr_out = (OpDtype*)outputs[0]->mutable_data();
     for(int i =0; i<num; ++i){
         offset = i*in_c*in_h*in_w;
         for(int j=_c_off;j<_c_end;++j){
