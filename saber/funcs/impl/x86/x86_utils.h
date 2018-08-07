e43f8cf4bc08c17096d17cf9249c8dcc8a862309
luoxiaocheng <luoxiaocheng@baidu.com>
2018-08-07 16:51:26 +0800

    fix bug in x86_utils.h (add type check)

--- saber/funcs/impl/x86/x86_utils.h
+++ saber/funcs/impl/x86/x86_utils.h
@@ -597,10 +597,14 @@ inline void yield_thread() { }
 // reorder weight layout from NCHW(oc, ic, kh, kw) to OIhw16i16o
 inline void weight_reorder_OIhw16i16o(Tensor<X86>& input,
                                       Tensor<X86>& output) {
+    CHECK_EQ(input.get_dtype(), AK_FLOAT) << "only support float type";
+    CHECK_EQ(output.get_dtype(), AK_FLOAT) << "only support float type";
     Shape shape = input.valid_shape();
     int oc_value = shape[0], ic_value = shape[1], kh_value = shape[2], kw_value = shape[3];
-    #pragma omp parallel for collapse(6) schedule(static)
 
+    #pragma omp parallel for collapse(6) schedule(static)
+    float* output_ptr = static_cast<float*>(output.mutable_data());
+    const float* input_ptr = static_cast<const float*>(input.data());
     for (int oc_idx = 0; oc_idx < oc_value / 16; ++oc_idx) {
         for (int ic_idx = 0; ic_idx < ic_value / 16; ++ic_idx) {
             for (int kh = 0; kh < kh_value; ++kh) {
@@ -615,7 +619,7 @@ inline void weight_reorder_OIhw16i16o(Tensor<X86>& input,
                                              kh * kw_value * 16 * 16 +
                                              kw * 16 * 16 + ic * 16 + oc;
 
-                            *((float*)output.mutable_data() + output_idx) = *((const float*)input.data() + input_idx);
+                            *(output_ptr + output_idx) = *(input_ptr + input_idx);
                         }
                     }
                 }
@@ -627,9 +631,13 @@ inline void weight_reorder_OIhw16i16o(Tensor<X86>& input,
 // reorder weight layout from NCHW(oc, ic, kh, kw) to OIhwi16o
 inline void weight_reorder_OIhwi16o(Tensor<X86>& input,
                                     Tensor<X86>& output) {
+    CHECK_EQ(input.get_dtype(), AK_FLOAT) << "only support float type";
+    CHECK_EQ(output.get_dtype(), AK_FLOAT) << "only support float type";
     Shape shape = input.shape();
-    #pragma omp parallel for collapse(5) schedule(static)
 
+    #pragma omp parallel for collapse(5) schedule(static)
+    float* output_ptr = static_cast<float*>(output.mutable_data());
+    const float* input_ptr = static_cast<const float*>(input.data());
     for (int oc_idx = 0; oc_idx < shape[0] / 16; ++oc_idx) {
         for (int kh = 0; kh < shape[2]; ++kh) {
             for (int kw = 0; kw < shape[3]; ++kw) {
@@ -643,7 +651,7 @@ inline void weight_reorder_OIhwi16o(Tensor<X86>& input,
                                          kw * shape[1] * 16 +
                                          ic * 16 + oc;
 
-                        *((float*)output.mutable_data() + output_idx) = *((const float*)input.data() + input_idx);
+                        *(output_ptr + output_idx) = *(input_ptr + input_idx);
                     }
                 }
             }
@@ -655,10 +663,13 @@ inline void weight_reorder_OIhwi16o(Tensor<X86>& input,
 // reorder weight layout from NCHW(oc, ic, kh, kw) to OIhwi8o
 inline void weight_reorder_OIhwi8o(Tensor<X86>& input,
                                    Tensor<X86>& output) {
+    CHECK_EQ(input.get_dtype(), AK_FLOAT) << "only support float type";
+    CHECK_EQ(output.get_dtype(), AK_FLOAT) << "only support float type";
     Shape shape = input.shape();
 
     #pragma omp parallel for collapse(5) schedule(static)
-
+    float* output_ptr = static_cast<float*>(output.mutable_data());
+    const float* input_ptr = static_cast<const float*>(input.data());
     for (int oc_idx = 0; oc_idx < shape[0] / 8; ++oc_idx) {
         for (int kh = 0; kh < shape[2]; ++kh) {
             for (int kw = 0; kw < shape[3]; ++kw) {
@@ -672,7 +683,7 @@ inline void weight_reorder_OIhwi8o(Tensor<X86>& input,
                                          kw * shape[1] * 8 +
                                          ic * 8 + oc;
 
-                        *((float *)output.mutable_data() + output_idx) = *((const float*)input.data() + input_idx);
+                        *(output_ptr + output_idx) = *(input_ptr + input_idx);
                     }
                 }
             }
@@ -683,11 +694,15 @@ inline void weight_reorder_OIhwi8o(Tensor<X86>& input,
 // reorder weight layout from NCHW to Goihw16g
 static void weight_reorder_Goihw16g(Tensor<X86>& input,
                                     Tensor<X86>& output) {
+    CHECK_EQ(input.get_dtype(), AK_FLOAT) << "only support float type";
+    CHECK_EQ(output.get_dtype(), AK_FLOAT) << "only support float type";
     Shape shape = input.shape();
     int g_value = shape[0], oc_value = shape[1], ic_value = shape[1], kh_value = shape[2],
         kw_value = shape[3];
-    #pragma omp parallel for collapse(6) schedule(static)
 
+    #pragma omp parallel for collapse(6) schedule(static)
+    float* output_ptr = static_cast<float*>(output.mutable_data());
+    const float* input_ptr = static_cast<const float*>(input.data());
     for (int g_idx = 0; g_idx < g_value / 16; ++g_idx) {
         for (int oc_idx = 0; oc_idx < oc_value; ++oc_idx) {
             for (int ic_idx = 0; ic_idx < ic_value; ++ic_idx) {
@@ -703,7 +718,7 @@ static void weight_reorder_Goihw16g(Tensor<X86>& input,
                                              ic_idx * kh_value * kw_value * 16 +
                                              kh * kw_value * 16 + kw * 16 + g;
 
-                            *((float*)output.mutable_data() + output_idx) = *((const float*)input.data() + input_idx);
+                            *(output_ptr + output_idx) = *(input_ptr + input_idx);
                         }
                     }
                 }
