c170c61e6ed09e0c58f8a4352db29736ca389688
MyPandaShaoxiang <txg4794@163.com>
2018-08-07 11:51:20 +0800

    add power head

--- saber/saber_funcs_param.h
+++ saber/saber_funcs_param.h
@@ -357,6 +357,24 @@ struct ReshapeParam {
         std::vector<int> shape_params;
         
 };
+    
+template<typename TargetType>
+struct PowerParam {
+        PowerParam() {}
+        PowerParam(float power, float scale, float shift) : power(power), scale(scale), shift(shift) {}
+        PowerParam(const PowerParam &right) : power(right.power), scale(right.scale), shift(right.shift) {}
+        bool operator==(const PowerParam &right) {
+            bool comp_eq = true;
+            comp_eq = comp_eq && (power == right.power);
+            comp_eq = comp_eq && (scale == right.scale);
+            comp_eq = comp_eq && (shift == right.shift);
+            return comp_eq;
+        }
+        float power;
+        float scale;
+        float shift;
+};
+
 
     
 }//namespace saber
