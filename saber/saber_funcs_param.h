6dde054b92cae43bd8809ce52487b7908b858b18
throneclay <throneclay@gmail.com>
2018-08-14 14:36:51 +0800

    add ctc_align param, apis

--- saber/saber_funcs_param.h
+++ saber/saber_funcs_param.h
@@ -181,6 +181,31 @@ private:
 
 };
 
+template <typename TargetType>
+struct CtcAlignParam {
+    CtcAlignParam() = default;
+    CtcAlignParam(int blank_in, bool merge_repeated_in)
+            : blank(blank_in)
+            , merge_repeated(merge_repeated_in)
+    {}
+    CtcAlignParam(const CtcAlignParam &right)
+            : blank(right.blank)
+            , merge_repeated(right.merge_repeated)
+    {}
+    CtcAlignParam &operator=(const CtcAlignParam &right) {
+        blank = right.blank;
+        merge_repeated = right.merge_repeated;
+        return *this;
+    }
+    bool operator==(const CtcAlignParam &right) {
+        bool comp_eq = true;
+        comp_eq = comp_eq && (blank == right.blank);
+        comp_eq = comp_eq && (merge_repeated == right.merge_repeated);
+        return comp_eq;
+    }
+    int blank;
+    bool merge_repeated;
+};
 
 template <typename TargetType>
 struct NormalizeParam {
