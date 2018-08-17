d995bde943c59357d9c161f06d3c2bb653a0d0d5
Shixiaowei02 <klsxw@126.com>
2018-08-17 16:57:12 +0800

    update gpu arch

--- tools/gpu_build.sh
+++ tools/gpu_build.sh
@@ -3,6 +3,13 @@
 ANAKIN_ROOT="$( cd "$(dirname "$0")"/.. ; pwd -P)"
 echo "-- Anakin root dir is: $ANAKIN_ROOT"
 
+# Choose GPU architecture.
+if [ "$1" == "sm50" ]; then
+    NV_ARCH="50"
+else
+    NV_ARCH="61"
+fi
+
 # build the target into gpu_build.
 BUILD_ROOT=$ANAKIN_ROOT/gpu_build
 
@@ -15,18 +22,19 @@ cd $BUILD_ROOT
 
 cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
-	-DUSE_ARM_PLACE=NO \
-	-DUSE_GPU_PLACE=YES \
-	-DUSE_X86_PLACE=NO \
-	-DBUILD_FAT_BIN=YES \
-	-DBUILD_WITH_UNIT_TEST=YES \
-   	-DUSE_PYTHON=OFF \
-	-DENABLE_DEBUG=NO \
-	-DENABLE_VERBOSE_MSG=NO \
-	-DDISABLE_ALL_WARNINGS=YES \
-	-DENABLE_NOISY_WARNINGS=NO \
+    -DUSE_ARM_PLACE=NO \
+    -DUSE_GPU_PLACE=YES \
+    -DUSE_X86_PLACE=NO \
+    -DBUILD_FAT_BIN=YES \
+    -DBUILD_WITH_UNIT_TEST=YES \
+    -DUSE_PYTHON=OFF \
+    -DENABLE_DEBUG=NO \
+    -DENABLE_VERBOSE_MSG=NO \
+    -DDISABLE_ALL_WARNINGS=YES \
+    -DENABLE_NOISY_WARNINGS=NO \
     -DUSE_OPENMP=NO\
-	-DBUILD_SHARED=YES
+    -DBUILD_SHARED=YES
+    -DSELECTED_SASS_TARGET_ARCH=$NV_ARCH \
 
 # build target lib or unit test.
 if [ "$(uname)" = 'Darwin' ]; then
