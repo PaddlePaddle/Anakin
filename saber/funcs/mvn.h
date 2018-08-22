2e843fb6758cf3c13a3d883f7c1ef5339ad24d46
luoxiaocheng <luoxiaocheng@baidu.com>
2018-08-22 18:37:06 +0800

    add mvn

--- saber/funcs/mvn.h
+++ saber/funcs/mvn.h
@@ -62,7 +62,8 @@ public:
 
         //! support inplace computation, output shape = input shape
         Shape output_shape = input[0]->valid_shape();
-        output[0]->set_shape(output_shape);
+        return output[0]->set_shape(output_shape);
+ //       return SaberSuccess;
     }
 
     virtual SaberStatus init_impl(ImplEnum implenum) override {
