bc5cdd9c4b804dd75128a4ce66f764f752afc4b7
throneclay <throneclay@gmail.com>
2018-08-27 15:00:03 +0800

    update saber funcs

--- saber/funcs/mat_mul.h
+++ saber/funcs/mat_mul.h
@@ -110,12 +110,6 @@ private:
         this->_best_impl = this->_impl[0];
     }
 
-    virtual void pick_best_runtime(Input_v input, Output_v output, \
-        Param_t& param, Context<TargetType> &ctx) override {
-        //! Fc only has saber implementation
-        this->_best_impl = this->_impl[0];
-    }
-
     virtual void pick_best_specify(ImplEnum implenum) override {
         //! Fc only has saber implementation
         this->_best_impl = this->_impl[0];
--- saber/funcs/transpose.h
+++ saber/funcs/transpose.h
@@ -89,12 +89,6 @@ private:
         this->_best_impl = this->_impl[0];
     }
 
-    virtual void pick_best_runtime(Input_v input, Output_v output, \
-        Param_t& param, Context<TargetType> &ctx) override {
-        //! Transpose only has saber implementation
-        this->_best_impl = this->_impl[0];
-    }
-
     virtual void pick_best_specify(ImplEnum implenum) override {
         //! Transpose only has saber implementation
         this->_best_impl = this->_impl[0];
