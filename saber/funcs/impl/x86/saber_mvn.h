a67dbb51aa9b5aec4c59122c7982136b1c96609e
luoxiaocheng <luoxiaocheng@baidu.com>
2018-08-29 16:23:59 +0800

    add mvn x86

---
+++ saber/funcs/impl/x86/saber_mvn.h
@@ -0,0 +1,61 @@
+/* Copyright (c) 2018 Anakin Authors, Inc. All Rights Reserved.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+#ifndef ANAKIN_SABER_FUNCS_IMPL_CUDA_SABER_MVN_H
+#define ANAKIN_SABER_FUNCS_IMPL_CUDA_SABER_MVN_H
+
+#include "saber/funcs/impl/impl_mvn.h"
+
+namespace anakin{
+
+namespace saber{
+
+template <DataType OpDtype>
+class SaberMvn<X86, OpDtype>: public ImplBase<X86, OpDtype, MvnParam<X86> > {
+
+public:
+    typedef typename DataTrait<X86, OpDtype>::Dtype OpDataType;
+
+    SaberMvn() {}
+
+    ~SaberMvn() {}
+
+    virtual SaberStatus init(const std::vector<Tensor<X86> *>& inputs,
+                             std::vector<Tensor<X86> *>& outputs,
+                             MvnParam<X86> &param,
+                             Context<X86> &ctx) {
+        this->_ctx = &ctx;
+        return create(inputs, outputs, param, ctx);
+    }
+
+    virtual SaberStatus create(const std::vector<Tensor<X86> *>& inputs,
+                               std::vector<Tensor<X86> *>& outputs,
+                               MvnParam<X86> &param,
+                               Context<X86> &ctx) {
+        return SaberSuccess;
+    }
+
+    virtual SaberStatus dispatch(const std::vector<Tensor<X86> *>& inputs,
+                                 std::vector<Tensor<X86> *>& outputs,
+                                 MvnParam<X86>  &param);
+
+
+};
+
+} //namespace saber
+
+} //namespace anakin
+
+#endif //ANAKIN_SABER_FUNCS_IMPL_CUDA_SABER_MVN_H
