b276f8500cbd5bec500071e6ac62fac46e97710c
throneclay <throneclay@gmail.com>
2018-09-03 14:01:50 +0800

    update x86 jit conv

--- saber/funcs/impl/x86/saber_conv.cpp
+++ saber/funcs/impl/x86/saber_conv.cpp
@@ -2,6 +2,9 @@
 #include "saber/funcs/impl/x86/saber_conv.h"
 #include "saber/funcs/impl/x86/saber_im2col_conv.h"
 #include "saber/funcs/impl/x86/jit_avx2_conv.h"
+#include "saber/funcs/impl/x86/jit_uni_dwconv.h"
+#include "saber/funcs/impl/x86/jit_avx512_conv1x1.h"
+#include "saber/funcs/impl/x86/jit_avx512_conv.h"
 
 namespace anakin {
 namespace saber {
@@ -23,10 +26,14 @@ SaberStatus SaberConv2D<X86, AK_FLOAT>::init(const std::vector<Tensor<X86> *>& i
     this->_ctx = &ctx;
     bool use_avx512 = mayiuse(avx512_common);
     bool use_avx2 = mayiuse(avx2);
-    if (use_avx2 && (outputs[0]->get_layout() == Layout_NCHW_C8)) {
+    if (use_avx512 && param.group == inputs[0]->channel() && param.group == outputs[0]->channel()) {
+        this->impl = new JitUniDWConv<AK_FLOAT>;
+    } else if (use_avx512 && param.weight()->height() == 1 && param.weight()->width() == 1) {
+        this->impl = new JitAvx512Conv1x1<AK_FLOAT>;
+    } else if (use_avx512 && outputs[0]->get_layout() == Layout_NCHW_C16) {
+        this->impl = new JitAvx512Conv<AK_FLOAT>;
+    } else if (use_avx2 && (outputs[0]->get_layout() == Layout_NCHW_C8)) {
         this->impl = new JitAvx2Conv<AK_FLOAT>;
-    } else if (use_avx512) {
-
     } else {
         this->impl = new SaberIm2colConv<AK_FLOAT>;
     }
