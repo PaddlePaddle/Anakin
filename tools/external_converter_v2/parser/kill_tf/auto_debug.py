d503eaaa8f5c45f6219280ecaa9d2bea17479dda
liujunjie <liujunjieqq33@126.com>
2018-09-04 11:57:59 +0800

    Merge branch 'developing' of https://github.com/PaddlePaddle/Anakin into developing
    # Conflicts:
    #	tools/external_converter_v2/parser/kill_fluid/parser_fluid.py

--- tools/external_converter_v2/parser/kill_tf/auto_debug.py
+++ tools/external_converter_v2/parser/kill_tf/auto_debug.py
@@ -6,12 +6,14 @@ from parse_tf_2_med import ParseTF2Med
 from med_graph import MedGraphUtil
 from tf_trans_util import load_graph
 from tf_util import TfUtil
+
+
 class AutoDebug:
 
-    def __init__(self,tf_model_path,ak_output_dir,workspace=None):
-        self.ak_perfix='record_'
-        self.tf_model_path=tf_model_path
-        self.ak_output_dir=os.path.dirname(ak_output_dir)
+    def __init__(self, tf_model_path, ak_output_dir, workspace=None):
+        self.ak_perfix = 'record_'
+        self.tf_model_path = tf_model_path
+        self.ak_output_dir = os.path.dirname(ak_output_dir)
         med_graph = ParseTF2Med(self.tf_model_path).parse()
         MedGraphUtil.solve(med_graph)
         self.med_graph = med_graph
@@ -21,13 +23,22 @@ class AutoDebug:
         #     self.workspace = workspace
         # print('workspace is ', self.workspace)
 
-
-    def _debug_graph(self,graph):
+    def _debug_graph(self, graph):
+        '''
+        print debug info
+        :param graph:
+        :return:
+        '''
         for i in graph.values():
             print(i)
         exit()
 
-    def _convert_name_tf2ak(self,tf_name):
+    def _convert_name_tf2ak(self, tf_name):
+        '''
+        conver name from tf 2 ak
+        :param tf_name:
+        :return:
+        '''
         ak_name = tf_name[:]
         for index, x in enumerate(tf_name):
             if x == '/':
@@ -35,14 +46,23 @@ class AutoDebug:
         return self.ak_perfix + ak_name
 
     def _find_ak_output(self):
+        '''
+        find output name in graph
+        :return:
+        '''
         result = {}
         for file_name in os.listdir(self.ak_output_dir):
-            real_path=os.path.join(self.ak_output_dir,file_name)
-            if not os.path.isdir(real_path) and file_name.startswith(self.ak_perfix) and not file_name.startswith(self.ak_perfix+'_'):
-                result[file_name]=[float(i.split(' ')[1]) for i in open(real_path,'r').readlines()]
+            real_path = os.path.join(self.ak_output_dir, file_name)
+            if not os.path.isdir(real_path) and file_name.startswith(self.ak_perfix) and not file_name.startswith(
+                    self.ak_perfix + '_'):
+                result[file_name] = [float(i.split(' ')[1]) for i in open(real_path, 'r').readlines()]
         return result
 
     def _creat_and_clear_workspace(self):
+        '''
+        remove temp workspace
+        :return:
+        '''
         if os.path.exists(self.workspace):
             assert os.path.isdir(self.workspace)
             ls = os.listdir(self.workspace)
@@ -52,60 +72,73 @@ class AutoDebug:
         else:
             os.mkdir(self.workspace)
 
-    def _prepare_data(self,med_graph):
-        input_cnt=0
-        inputs={}
+    def _prepare_data(self, med_graph):
+        '''
+        gen input data
+        :param med_graph:
+        :return:
+        '''
+        input_cnt = 0
+        inputs = {}
         result = {}
         np.random.seed(1234567)
         for i in med_graph.values():
             if i['ak_type'] == 'Input':
-                shape=i['output'][0]['shape']
-                size=1
+                shape = i['output'][0]['shape']
+                size = 1
                 for k in shape:
-                    size*=k
-                tensor=np.random.randn(size).reshape(shape)
-                inputs['input_'+str(input_cnt)]=tensor
-                result[i['name']+':0']=tensor
-                input_cnt+=1
+                    size *= k
+                tensor = np.random.randn(size).reshape(shape)
+                inputs['input_' + str(input_cnt)] = tensor
+                result[i['name'] + ':0'] = tensor
+                input_cnt += 1
 
         for i in inputs.keys():
             import struct
-            with open(os.path.join(self.ak_output_dir,i), "wb") as file:
+            with open(os.path.join(self.ak_output_dir, i), "wb") as file:
                 x_value_new = inputs[i].transpose((0, 3, 1, 2))
                 for value in x_value_new.flatten():
                     file.write(struct.pack('f', value))
 
         return result
 
-
     def run_ak(self):
+        '''
+        run ak mode
+        :return:
+        '''
         self._prepare_data(self.med_graph)
 
     def run(self):
-        inputs = self._prepare_data(self.med_graph)
-        akopname2ak_out=self._find_ak_output()
-        for i in akopname2ak_out:
-            print(i)
-        compare_dic={}
-        for node in self.med_graph.values():
-            print(node['name'], node['ak_type'])
-            if node['ak_type'] is not None and node['type'] is not None:
-
-                name=node['name']
-                if node.get('fusion_out_name') is not None:
-                    name=node['fusion_out_name']
-                ak_name=self._convert_name_tf2ak(name)
-                print('key ',ak_name)
-                if akopname2ak_out.get(ak_name) is not None:
-                    compare_dic[name+':0']=akopname2ak_out[ak_name]
-        outlist=compare_dic.keys()
-        out=TfUtil.tf_run_model(self.tf_model_path,inputs,outlist)
-        for index,name in enumerate(outlist):
-            correct=out[index].transpose()
-            ak_result=akopname2ak_out[name]
-            assert len()
-            for i in range(len(ak_result)):
-                if abs()
+        '''
+        run debug mode
+        :return:
+        '''
+        pass
+        # inputs = self._prepare_data(self.med_graph)
+        # akopname2ak_out=self._find_ak_output()
+        # for i in akopname2ak_out:
+        #     print(i)
+        # compare_dic={}
+        # for node in self.med_graph.values():
+        #     print(node['name'], node['ak_type'])
+        #     if node['ak_type'] is not None and node['type'] is not None:
+        #
+        #         name=node['name']
+        #         if node.get('fusion_out_name') is not None:
+        #             name=node['fusion_out_name']
+        #         ak_name=self._convert_name_tf2ak(name)
+        #         print('key ',ak_name)
+        #         if akopname2ak_out.get(ak_name) is not None:
+        #             compare_dic[name+':0']=akopname2ak_out[ak_name]
+        # outlist=compare_dic.keys()
+        # out=TfUtil.tf_run_model(self.tf_model_path,inputs,outlist)
+        # for index,name in enumerate(outlist):
+        #     correct=out[index].transpose()
+        #     ak_result=akopname2ak_out[name]
+        #     assert len()
+        #     for i in range(len(ak_result)):
+        #         if abs()
         # tensor_shape = {}
         # graph={}
         # for node in tf_ops:
@@ -116,9 +149,10 @@ class AutoDebug:
         #     graph[node.name]={'name':node.name,'input':input_names,'output':output_names}
         # self._fix_self_output(graph,tensor_shape)
         # self._debug_graph(graph)
-ak_work_space='/home/ljj/docker_mount_dev2/anakin2_developing_parser/build/'
 
-debug=AutoDebug('./resnet_model/frozen_resnet_v1_50.pb',ak_work_space)
+
+ak_work_space = '/home/ljj/docker_mount_dev2/anakin2_developing_parser/build/'
+
+debug = AutoDebug('./resnet_model/frozen_resnet_v1_50.pb', ak_work_space)
 debug.run_ak()
 debug.run()
-
--- tools/external_converter_v2/parser/kill_tf/med_graph.py
+++ tools/external_converter_v2/parser/kill_tf/med_graph.py
@@ -1,15 +1,26 @@
 import numpy as np
 
 
-
 class MedNodeUtil:
 
     @staticmethod
     def new_med_node():
-        return {'name':None,'ak_type': None, 'input': [], 'output': [], 'ak_attr': {},'type':None,'med_visted':False}
+        '''
+        return instance of empty standard med graph node
+        :return:
+        '''
+        return {'name': None, 'ak_type': None, 'input': [], 'output': [], 'ak_attr': {}, 'type': None,
+                'med_visted': False}
 
     @staticmethod
     def replace_name_with_list(origin_list, name, replace_list):
+        '''
+        replace name in input or output with replace_list
+        :param origin_list:
+        :param name:
+        :param replace_list:
+        :return:
+        '''
         new_list = []
         for index, ori_object in enumerate(origin_list):
             if ori_object['name'] == name:
@@ -23,6 +34,12 @@ class MedNodeUtil:
 
     @staticmethod
     def retain_input(node, input_list):
+        '''
+        remove node input except element in input_list
+        :param node:
+        :param input_list:
+        :return:
+        '''
         new_input = []
         for index, node_object in enumerate(node['input']):
             if node_object in input_list:
@@ -31,17 +48,35 @@ class MedNodeUtil:
         node['input'] = new_input
 
     @staticmethod
-    def redirecto_outputs_input_to_this(node,graph,this_name,this_shape):
+    def redirecto_outputs_input_to_this(node, graph, this_name, this_shape):
+        '''
+        get node_x in node`s outputs
+        make node_x`s inputs reference to node
+        :param node:
+        :param graph:
+        :param this_name:
+        :param this_shape:
+        :return:
+        '''
         for i in node['output']:
-            tar_node=graph[i['name']]
-            tar_node['input']=MedNodeUtil.replace_name_with_list(tar_node['input'],node['name'],[{'name':this_name,'shape':this_shape}])
+            tar_node = graph[i['name']]
+            tar_node['input'] = MedNodeUtil.replace_name_with_list(tar_node['input'], node['name'],
+                                                                   [{'name': this_name, 'shape': this_shape}])
 
 
+MedGraph_Input_Cnt = 0
 
-MedGraph_Input_Cnt=0
+
 class MedGraphUtil:
     @staticmethod
     def append_node(father_node, son_node, graph):
+        '''
+        add the son_node after father_node in graph
+        :param father_node:
+        :param son_node:
+        :param graph:
+        :return:
+        '''
         output = father_node['output']
         son_shape = father_node['output'][0]['shape']
         son_node['input'] = [{'name': father_node['name'], 'shape': son_shape}]
@@ -54,7 +89,13 @@ class MedGraphUtil:
         graph[son_node['name']] = son_node
 
     @staticmethod
-    def check_one_of_input_is_const(node,graph):
+    def check_one_of_input_is_const(node, graph):
+        '''
+        check one of input is const
+        :param node:
+        :param graph:
+        :return:
+        '''
         for i in node['input']:
             if graph[i['name']]['type'] == 'Const':
                 return True
@@ -62,26 +103,45 @@ class MedGraphUtil:
 
     @staticmethod
     def _auto_split(med_node, med_graph):
+        '''
+        add split to node which output size >=2
+        :param med_node:
+        :param med_graph:
+        :return:
+        '''
         output = med_node['output']
         if len(output) > 1:
             split_node = MedNodeUtil.new_med_node()
-            split_node['name']=med_node['name']+'_split#'
-            split_node['ak_type']='Split'
-            split_node['ak_attr']['split_num'] =len(output)
-            MedGraphUtil.append_node(med_node,split_node,graph=med_graph)
+            split_node['name'] = med_node['name'] + '_split#'
+            split_node['ak_type'] = 'Split'
+            split_node['ak_attr']['split_num'] = len(output)
+            MedGraphUtil.append_node(med_node, split_node, graph=med_graph)
         pass
 
     @staticmethod
     def _auto_input_name(med_node, med_graph):
-        assert med_node['ak_type']=='Input'
-        old_name=med_node['name']
-        med_node['name']='input_'+str(MedGraph_Input_Cnt)
+        '''
+        gen input name
+        :param med_node:
+        :param med_graph:
+        :return:
+        '''
+        assert med_node['ak_type'] == 'Input'
+        old_name = med_node['name']
+        med_node['name'] = 'input_' + str(MedGraph_Input_Cnt)
         for i in med_node['output']:
-            out_node=med_graph[i['name']]
-            out_node['input']=MedNodeUtil.replace_name_with_list(out_node['input'],old_name,[{'name':med_node['name'],'shape':i['shape']}])
+            out_node = med_graph[i['name']]
+            out_node['input'] = MedNodeUtil.replace_name_with_list(out_node['input'], old_name,
+                                                                   [{'name': med_node['name'], 'shape': i['shape']}])
 
     @staticmethod
     def _fusionScale(med_node, med_graph):
+        '''
+        fusion scale node after convolution node
+        :param med_node:
+        :param med_graph:
+        :return:
+        '''
         if len(med_node['input']) == 1:
             input_node = med_graph[med_node['input'][0]['name']]
             med_ak_attr = med_node['ak_attr']
@@ -100,14 +160,22 @@ class MedGraphUtil:
                 else:
                     input_attr['bias_weights'] = med_ak_attr['bias_weights']
                 med_node['ak_type'] = None
-                input_node['output'] = MedNodeUtil.replace_name_with_list(input_node['output'], med_node['name'], med_node['output'])
-                MedNodeUtil.redirecto_outputs_input_to_this(med_node,med_graph,input_node['name'],med_node['input'][0]['shape'])
-                input_node['fusion_out_name']=med_node['name']
+                input_node['output'] = MedNodeUtil.replace_name_with_list(input_node['output'], med_node['name'],
+                                                                          med_node['output'])
+                MedNodeUtil.redirecto_outputs_input_to_this(med_node, med_graph, input_node['name'],
+                                                            med_node['input'][0]['shape'])
+                input_node['fusion_out_name'] = med_node['name']
 
         pass
 
     @staticmethod
     def _all_search_table(graph, table):
+        '''
+        search template for dict
+        :param graph:
+        :param table:
+        :return:
+        '''
         for tf_node in graph.values():
             if tf_node['med_visted']:
                 continue
@@ -117,6 +185,12 @@ class MedGraphUtil:
 
     @staticmethod
     def _all_search_fusion(graph, fusion_func):
+        '''
+        search template for func
+        :param graph:
+        :param fusion_func:
+        :return:
+        '''
         for tf_node in graph.values():
             if tf_node['med_visted']:
                 continue
@@ -125,14 +199,24 @@ class MedGraphUtil:
 
     @staticmethod
     def solve(med_graph):
+        '''
+        do fusion and adjust for med graph that we can convert med graph to ak graph
+        :param med_graph:
+        :return:
+        '''
         for node in med_graph.values():
             node['med_visted'] = False
         MedGraphUtil._all_search_table(med_graph, {'Scale': MedGraphUtil._fusionScale})
         MedGraphUtil._all_search_fusion(med_graph, MedGraphUtil._auto_split)
-        MedGraphUtil._all_search_table(med_graph, {'Input':MedGraphUtil._auto_input_name})
+        MedGraphUtil._all_search_table(med_graph, {'Input': MedGraphUtil._auto_input_name})
 
     @staticmethod
     def search_output_list(graph):
+        '''
+        search output list in recursive method
+        :param graph:
+        :return:
+        '''
         output_list = set()
         graph_cp = graph.copy()
 
