60fba5260ee342ed5fd297893d38372bb0c5b1da
shixiaowei02 <shixiaowei02@baidu.com>
2018-09-07 21:43:19 +0800

    fix conflict

--- saber/funcs/activation.h
+++ saber/funcs/activation.h
@@ -29,15 +29,15 @@
 #include "saber/funcs/impl/x86/saber_activation.h"
 #endif
 
-// #ifdef USE_AMD
-// #include "saber/funcs/impl/amd/saber_activation.h"
-// #endif
+#ifdef AMD_GPU 
+#include "saber/funcs/impl/amd/saber_activation.h"
+#endif
 
 #ifdef USE_ARM_PLACE
 #include "saber/funcs/impl/arm/saber_activation.h"
 #endif
 
-#ifdef USE_BM
+#ifdef USE_BM_PLACE 
 #include "saber/funcs/impl/bm/vender_activation.h"
 #endif
 
