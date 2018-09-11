a0a9618c4c055cc6f8c4bae1b9ed28355e5a395d
guangzhixie <guangzhi.xie@berkeley.edu>
2018-09-11 14:34:01 +0800

    uncomment bm conv test

--- test/saber/test_saber_conv.cpp
+++ test/saber/test_saber_conv.cpp
@@ -11,7 +11,6 @@ using namespace anakin::saber;
 #define CHECK_RESULT
 //#define CHECK_SPEED
 
-#if 0
 #ifdef USE_BM_PLACE
 TEST(TestSaberFunc, test_saber_conv_results_bm) {
     Env<BM>::env_init();
@@ -69,7 +68,6 @@ TEST(TestSaberFunc, test_saber_conv_results_bm) {
 
     }
 }
-#endif
 #endif
 
 TEST(TestSaberFunc, test_saber_conv_results) {
