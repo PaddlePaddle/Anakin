94ea527f8420e6ffcf950e57d4dc688ffa80861c
Vin Huang <vin.huang@amd.com>
2018-09-14 04:19:24 +0800

    Disable AMD ops
    Change-Id: I784bc628c519e9477931fd6453757393afd8cb98

--- saber/funcs/softmax.h
+++ saber/funcs/softmax.h
@@ -24,7 +24,7 @@
 #include "saber/funcs/impl/cuda/vender_softmax.h"
 #endif
 #ifdef AMD_GPU
-#include "saber/funcs/impl/amd/saber_softmax.h"
+//#include "saber/funcs/impl/amd/saber_softmax.h"
 #endif
 #ifdef USE_X86_PLACE
 #include "saber/funcs/impl/x86/saber_softmax.h"
