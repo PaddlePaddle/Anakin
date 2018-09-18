46aa1545a65d53e9d2055b63ee3802c5a57eb9df
guangzhixie <guangzhi.xie@berkeley.edu>
2018-09-18 16:05:37 +0800

    BM conv host implementation

---
+++ saber/funcs/impl/bm/device/bmk_conv.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void bm_conv_fwd(bm_api_conv_forward conv_param) {
+    
+}
--- saber/funcs/impl/bm/device/bmkernel_base.c
+++ saber/funcs/impl/bm/device/bmkernel_base.c
@@ -1,5 +1,6 @@
 #include "bmkernel_base.h"
 #include "bm_config.h"
+#include "bmk_conv.c"
 #include <stdio.h>
 /**
  * bmkernel_func is the user entry to BMKERNEL just like "main" to some applications.
@@ -12,12 +13,15 @@ int bmkernel_func(void *args)
 {
     bmkernel_api_base* param = (bmkernel_api_base *)args;
     switch (param->op) {
-        case ACTIVATION:
+        case ACTIVATION: {
             // bm_activation_fwd(param)
             break;
-        case CONV:
-            // bm_conv_fwd(param)
+        }
+        case CONV: {
+            bm_api_conv_forward* api = (bm_api_conv_forward *)param->opParam;
+            bm_conv_fwd(*api);
             break;
+        }
         default:
             printf("op %d is not supported by BM yet.\n", param->op);
     }
