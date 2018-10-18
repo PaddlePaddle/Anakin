a743451f947feb53d63e11de1bc8bbd36d857187
guangzhixie <guangzhi.xie@berkeley.edu>
2018-10-18 17:03:44 +0800

    update getcwd test

--- saber/funcs/impl/bm/vender_conv.cpp
+++ saber/funcs/impl/bm/vender_conv.cpp
@@ -282,8 +282,12 @@ SaberStatus VenderConv2D<BM, AK_FLOAT>::\
     LOG(INFO)<<"BM Conv ends...";
     print_tensor(*outputs[0]);
 
-    char *buffer = getcwd(NULL, 0);
-    LOG(INFO)<<buffer;
+    char *buffer;
+    if ((buffer = getcwd(NULL, 0)) == NULL){
+      perror("getcwd error");
+    } else {
+      printf("%s\n", buffer);
+    }
 
     return SaberSuccess;
 }
