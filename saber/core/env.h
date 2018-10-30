d5a23f4a872e430862f5737cd14ff72bd11132f0
guangzhixie <guangzhi.xie@berkeley.edu>
2018-10-30 15:31:30 +0800

    Revert "Issue fix"
    This reverts commit 56f0241dffce0eb43611abc22121560b1fc85be5.

--- saber/core/env.h
+++ saber/core/env.h
@@ -36,6 +36,9 @@ public:
         if (devs.size() > 0){
             return;
         }
+#ifdef USE_BM_PLACE
+        API::init_handle();
+#endif
         int count = 0;
         API::get_device_count(count);
         if (count == 0) {
@@ -53,49 +56,13 @@ public:
     }
 private:
     Env(){}
-};
 
 #ifdef USE_BM_PLACE
-template <>
-class Env<BM> {
-public:
-    typedef TargetWrapper<BM> API;
-    typedef std::vector<Device<BM>> Devs;
-    static Devs& cur_env() {
-        static Devs* _g_env = new Devs();
-        return *_g_env;
+    ~Env(){
+        API::deinit_handle();
     }
-    static void env_init(int max_stream = 4){
-        Devs& devs = cur_env();
-        if (devs.size() > 0){
-            return;
-        }
-        API::init_handle();
-
-        int count = 0;
-        API::get_device_count(count);
-        if (count == 0) {
-            CHECK(false) << "no device found!";
-        } else {
-            LOG(INFO) << "found " << count << " device(s)";
-        }
-        int cur_id = API::get_device_id();
-        for (int i = 0; i < count; i++) {
-            API::set_device(i);
-            devs.push_back(Device<TargetType>(max_stream));
-        }
-        API::set_device(cur_id);
-        LOG(INFO)<<"dev size = "<<devs.size();
-    }
-private:
-    Env(){}
-};
-
-Env<BM>::~Env(){
-    API::deinit_handle();
-}
-
 #endif
+};
 
 #ifdef AMD_GPU 
 typedef std::list<cl_event> cl_event_list;
