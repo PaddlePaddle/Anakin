13aa1f73ced2529159cbbb3179bf3b0574a258f6
wunhuang <wun-guo.huang@amd.com>
2018-11-08 14:16:43 +0800

    Download customized miopen code while compiling Anakin
    Change-Id:Ib6684724bfee04de1601f7f1c82c151d35dda690

--- cmake/external/miopen.cmake
+++ cmake/external/miopen.cmake
@@ -14,27 +14,23 @@
 include(ExternalProject)
 
 set(MIOPEN_PROJECT       extern_miopen)
-set(MIOPEN_PREFIX_DIR    ${ANAKIN_TEMP_THIRD_PARTY_PATH}/miopen)
+set(MIOPEN_PREFIX_DIR    ${ANAKIN_THIRD_PARTY_PATH})
 set(MIOPEN_INSTALL_ROOT  ${ANAKIN_THIRD_PARTY_PATH}/miopen)
-set(MIOPEN_SOURCE_DIR    ${MIOPEN_PREFIX_DIR}/src/${MIOPEN_PROJECT})
-set(MIOPEN_BINARY_DIR    ${MIOPEN_PREFIX_DIR}/src/${MIOPEN_PROJECT}-build)
+set(MIOPEN_SOURCE_DIR    ${ANAKIN_THIRD_PARTY_PATH}/src/${MIOPEN_PROJECT})
+set(MIOPEN_STAMP_DIR     ${ANAKIN_TEMP_THIRD_PARTY_PATH}/${MIOPEN_PROJECT}/stamp)
+set(MIOPEN_BINARY_DIR    ${ANAKIN_TEMP_THIRD_PARTY_PATH}/${MIOPEN_PROJECT}/build)
 set(MIOPEN_LIB           ${MIOPEN_INSTALL_ROOT}/lib/libMIOpen.so  CACHE FILEPATH "miopen library." FORCE)
-
-if(NOT Boost_FOUND)
-    set(BOOST_ROOT       ${BOOST_INSTALL_ROOT} CACHE FILEPATH "boost library/" FORCE)
-endif()
 
 message(STATUS "Scanning external modules ${Green}MIOPEN${ColourReset} ...")
 
 ExternalProject_Add(
     ${MIOPEN_PROJECT}
-    GIT_REPOSITORY        https://github.com/ROCmSoftwarePlatform/MIOpen.git
-    GIT_TAG               1.4.2
-    #URL                   https://github.com/ROCmSoftwarePlatform/MIOpen/archive/1.4.2.zip
-    #URL_HSA               SHA256=8c6dd7b3030c2ca3aea68f070daf5a55ce6a0e58642fa466bf2a09eaf51613aa
+    GIT_REPOSITORY        https://github.com/ROCmSoftwarePlatform/Anakin.git
+    GIT_TAG               anakin-amd_miopen
     PREFIX                ${MIOPEN_PREFIX_DIR}
-    CMAKE_ARGS            -DMIOPEN_BACKEND=OpenCL -DCMAKE_INSTALL_PREFIX=${MIOPEN_INSTALL_ROOT} -DCMAKE_INSTALL_LIBDIR=lib -DBOOST_ROOT=${BOOST_ROOT}
-    #LOG_DOWNLOAD          1
+    STAMP_DIR             ${MIOPEN_STAMP_DIR}
+    BINARY_DIR            ${MIOPEN_BINARY_DIR}
+    CMAKE_ARGS            -DMIOPEN_BACKEND=OpenCL -DCMAKE_INSTALL_PREFIX=${MIOPEN_INSTALL_ROOT} -DCMAKE_INSTALL_LIBDIR=lib
     LOG_BUILD             1
     
 )
@@ -48,6 +44,7 @@ ExternalProject_Add_Step(
     EXCLUDE_FORM_MAIN 1
     LOG               1
 )
+
 include_directories(${MIOPEN_INSTALL_ROOT}/include)
 add_library(miopen SHARED IMPORTED GLOBAL)
 SET_PROPERTY(TARGET miopen PROPERTY IMPORTED_LOCATION ${MIOPEN_LIB})
