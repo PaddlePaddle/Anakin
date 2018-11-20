3cfc846dda6bba5d9abac045339f004da3b0c860
ChingMin Chen <chingmin.chen@amd.com>
2018-11-20 18:14:52 +0800

    Optimize performance of fc
    Change-Id: Ie7300eafe7005e9bf9ec95ba201b4a17fe164abf

--- saber/funcs/impl/amd/saber_fc.cpp
+++ saber/funcs/impl/amd/saber_fc.cpp
@@ -54,25 +54,25 @@ SaberStatus SaberFc<AMD, OpDtype>::init(
 #define VGG16_FC7_NT_LOCAL_WORK_SIZE_BS32 (256)
 #define VGG16_FC7_NT_GLOBAL_WORK_SIZE_BS32 (4096 * 16)
 
-#define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS1 (256)
-#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS1 (8192 * 2)
-#define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS2 (128)
-#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS2 (8192 * 4)
+#define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS1 (64)
+#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS1 (64 * 64 * 16)
+#define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS2 (64)
+#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS2 (64 * 64 * 16)
 #define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS4 (64)
-#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS4 (8192 * 4)
+#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS4 (64 * 64 * 16)
 #define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS8 (64)
-#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS8 (8192 * 8)
+#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS8 (64 * 64 * 16)
 #define VGG16_FC8_NT_LOCAL_WORK_SIZE_BS32 (64)
-#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS32 (8192 * 8)
+#define VGG16_FC8_NT_GLOBAL_WORK_SIZE_BS32 (64 * 64 * 16)
 
-#define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS1 (256)
-#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS1 (8192 * 2)
-#define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS2 (128)
-#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS2 (8192 * 4)
+#define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS1 (64)
+#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS1 (64 * 64 * 16)
+#define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS2 (64)
+#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS2 (64 * 64 * 16)
 #define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS4 (64)
-#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS4 (8192 * 4)
+#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS4 (64 * 64 * 16)
 #define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS8 (64)
-#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS8 (8192 * 8)
+#define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS8 (64 * 64 * 16)
 #define RESNET_FC1000_NT_LOCAL_WORK_SIZE_BS32 (64)
 #define RESNET_FC1000_NT_GLOBAL_WORK_SIZE_BS32 (8192 * 8)
 
@@ -108,16 +108,16 @@ SaberStatus SaberFc<AMD, OpDtype>::init(
 #define VGG16_FC7_NT_KERNEL_FILE_NAME_BS4 "InnerProductBNTFC7M4.cl"
 #define VGG16_FC7_NT_KERNEL_FILE_NAME_BS8 "InnerProductBNTFC7M8.cl"
 #define VGG16_FC7_NT_KERNEL_FILE_NAME_BS32 "InnerProductBNTFC7M32.cl"
-#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS1 "InnerProductBNTFC8M1.cl"
-#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS2 "InnerProductBNTFC8M2.cl"
-#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS4 "InnerProductBNTFC8M4.cl"
-#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS8 "InnerProductBNTFC8M8.cl"
+#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS1 "Conv1x1FC7.cl"
+#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS2 "Conv1x1FC7.cl"
+#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS4 "Conv1x1FC7.cl"
+#define VGG16_FC8_NT_KERNEL_FILE_NAME_BS8 "Conv1x1FC7.cl"
 #define VGG16_FC8_NT_KERNEL_FILE_NAME_BS32 "InnerProductBNTFC8M32.cl"
 
-#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS1 "InnerProductBNTFC1000M1.cl"
-#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS2 "InnerProductBNTFC1000M2.cl"
-#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS4 "InnerProductBNTFC1000M4.cl"
-#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS8 "InnerProductBNTFC1000M8.cl"
+#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS1 "Conv1x1FC7.cl"
+#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS2 "Conv1x1FC7.cl"
+#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS4 "Conv1x1FC7.cl"
+#define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS8 "Conv1x1FC7.cl"
 #define RESNET_FC1000_NT_KERNEL_FILE_NAME_BS32 "InnerProductBNTFC1000M32.cl"
 
 #define YOLO_FC25_NT_KERNEL_FILE_NAME_BS1 "InnerProductBNTFC25M1.cl"
@@ -154,11 +154,11 @@ SaberStatus SaberFc<AMD, OpDtype>::create(
     ALOGD("create");
 
     ALOGI("AMD Summary: input size N " << inputs[0]->num() << " C " << inputs[0]->channel()
-        << " H " << inputs[0]->height() << " W " << inputs[0]->width());
+          << " H " << inputs[0]->height() << " W " << inputs[0]->width());
 
     ALOGI("AMD Summary: op param is_transpose_weights " << param.is_transpose_weights
-        << " num_output " << param.num_output
-        << " axis " << param.axis);
+          << " num_output " << param.num_output
+          << " axis " << param.axis);
 
     this->_ctx   = &ctx;
     this->_param = &param;
@@ -556,8 +556,13 @@ SaberStatus SaberFc<AMD, OpDtype>::create(
 
     if (optmized) {
         // set comp_options...
-        kernelInfo.comp_options = std::string(" -DM=") + std::to_string(M) + std::string(" -DN=")
-                                  + std::to_string(N) + std::string(" -DK=") + std::to_string(K);
+        kernelInfo.comp_options = std::string(" -DSTRIDE=") + std::to_string(inputs[0]->channel()) +
+                                  std::string(" -DN=") + std::to_string(inputs[0]->num());
+
+        if (param.bias != nullptr && param.bias->valid_size() > 0) {
+            kernelInfo.comp_options += std::string(" -DBIAS");
+        }
+
         kernelInfo.kernel_type = SABER;
 
         kptr = CreateKernel(inputs[0]->device_id(), &kernelInfo);
@@ -632,11 +637,20 @@ SaberStatus SaberFc<AMD, OpDtype>::dispatch(
                 return SaberInvalidValue;
             }
 
-            err = _kernels_ptr[0].get()->SetKernelArgs(
-                      (PtrDtype)memObjects[0],
-                      (PtrDtype)memObjects[1],
-                      (PtrDtype)memObjects[2],
-                      (PtrDtype)memObjects[3]);
+            if (param.bias != nullptr && param.bias->valid_size() > 0) {
+                err = _kernels_ptr[0].get()->SetKernelArgs(
+                          (PtrDtype)memObjects[0],
+                          (PtrDtype)memObjects[1],
+                          (PtrDtype)memObjects[2],
+                          (PtrDtype)memObjects[3],
+                          1.0f);
+            } else {
+                err = _kernels_ptr[0].get()->SetKernelArgs(
+                          (PtrDtype)memObjects[0],
+                          (PtrDtype)memObjects[1],
+                          (PtrDtype)memObjects[3],
+                          1.0f);
+            }
 
             if (!err) {
                 ALOGE("Fail to set kernel args :" << err);
