5297d6a863162197911f0341be448c83db362612
Vin Huang <vin.huang@amd.com>
2018-12-15 03:38:34 +0800

    mvn modify log
    Change-Id: I999647b73296d5b8ca4e6b52b560134b8d51316e

--- saber/funcs/impl/amd/saber_mvn.cpp
+++ saber/funcs/impl/amd/saber_mvn.cpp
@@ -21,10 +21,10 @@ typedef TargetWrapper<AMD> AMD_API;
 
 template <DataType OpDtype>
 SaberStatus SaberMvn<AMD, OpDtype>::init(
-        const std::vector<Tensor<AMD>*>& inputs,
-        std::vector<Tensor<AMD>*>& outputs,
-        MvnParam<AMD>& param,
-        Context<AMD>& ctx) {
+    const std::vector<Tensor<AMD>*>& inputs,
+    std::vector<Tensor<AMD>*>& outputs,
+    MvnParam<AMD>& param,
+    Context<AMD>& ctx) {
 
     this->_ctx = &ctx;
     return create(inputs, outputs, param, ctx);
@@ -32,26 +32,32 @@ SaberStatus SaberMvn<AMD, OpDtype>::init(
 
 template <DataType OpDtype>
 SaberStatus SaberMvn<AMD, OpDtype>::create(
-        const std::vector<Tensor<AMD>*>& inputs,
-        std::vector<Tensor<AMD>*>& outputs,
-        MvnParam<AMD>& param,
-        Context<AMD>& ctx) {
+    const std::vector<Tensor<AMD>*>& inputs,
+    std::vector<Tensor<AMD>*>& outputs,
+    MvnParam<AMD>& param,
+    Context<AMD>& ctx) {
     const int count = outputs[0]->valid_size();
     int num         = inputs[0]->num() * inputs[0]->channel();
     int inner_dim   = inputs[0]->height() * inputs[0]->width();
+
     if (param.across_channels) {
         num = inputs[0]->num();
         // inner_dim *= inputs[0]->channel();
     }
+
     Shape shape = inputs[0]->valid_shape();
+
     for (int i = 0; i < shape.size(); i++) {
         shape[i] = 1;
     }
+
     shape[0] = num;
     _mean.reshape(shape);
+
     if (param.normalize_variance) {
         _sd.reshape(shape);
     }
+
     AMDKernelPtr kptr;
     KernelInfo kernelInfo;
     kernelInfo.wk_dim      = 3;
@@ -64,20 +70,24 @@ SaberStatus SaberMvn<AMD, OpDtype>::create(
         kernelInfo.g_wk        = {256, (inner_dim + 256 - 1) / 256, num};
 
         kptr = CreateKernel(inputs[0]->device_id(), &kernelInfo);
+
         if (!kptr.get()->isInit()) {
-            ALOGE("Failed to create kernel");
+            LOG(ERROR) << "Failed to create kernel";
             return SaberInvalidValue;
         }
+
         _kernels_ptr.push_back(kptr);
 
         kernelInfo.kernel_name = "normalize_square";
         kernelInfo.l_wk        = {256, 1, 1};
         kernelInfo.g_wk        = {256, (inner_dim + 256 - 1) / 256, num};
         kptr                   = CreateKernel(inputs[0]->device_id(), &kernelInfo);
+
         if (!kptr.get()->isInit()) {
-            ALOGE("Failed to create kernel");
+            LOG(ERROR) << "Failed to create kernel";
             return SaberInvalidValue;
         }
+
         _kernels_ptr.push_back(kptr);
     } else {
 
@@ -86,38 +96,43 @@ SaberStatus SaberMvn<AMD, OpDtype>::create(
         kernelInfo.g_wk        = {256, (inner_dim + 256 - 1) / 256, num};
 
         kptr = CreateKernel(inputs[0]->device_id(), &kernelInfo);
+
         if (!kptr.get()->isInit()) {
-            ALOGE("Failed to create kernel");
+            LOG(ERROR) << "Failed to create kernel";
             return SaberInvalidValue;
         }
+
         _kernels_ptr.push_back(kptr);
 
         kernelInfo.kernel_name = "normalize";
         kernelInfo.l_wk        = {256, 1, 1};
         kernelInfo.g_wk        = {256, (inner_dim + 256 - 1) / 256, num};
         kptr                   = CreateKernel(inputs[0]->device_id(), &kernelInfo);
+
         if (!kptr.get()->isInit()) {
-            ALOGE("Failed to create kernel");
+            LOG(ERROR) << "Failed to create kernel";
             return SaberInvalidValue;
         }
+
         _kernels_ptr.push_back(kptr);
     }
 
-    ALOGD("COMPLETE CREATE KERNEL");
+    LOG_IF_S(INFO, ENABLE_AMD_DEBUG_LOG) << "COMPLETE CREATE KERNEL";
 
     return SaberSuccess;
 }
 
 template <DataType OpDtype>
 SaberStatus SaberMvn<AMD, OpDtype>::dispatch(
-        const std::vector<Tensor<AMD>*>& inputs,
-        std::vector<Tensor<AMD>*>& outputs,
-        MvnParam<AMD>& param) {
+    const std::vector<Tensor<AMD>*>& inputs,
+    std::vector<Tensor<AMD>*>& outputs,
+    MvnParam<AMD>& param) {
     // To get the commpute command queue
     AMD_API::stream_t cm = this->_ctx->get_compute_stream();
 
     int num       = inputs[0]->num() * inputs[0]->channel();
     int inner_dim = inputs[0]->height() * inputs[0]->width();
+
     if (param.across_channels) {
         num = inputs[0]->num();
         inner_dim *= inputs[0]->channel();
@@ -128,76 +143,82 @@ SaberStatus SaberMvn<AMD, OpDtype>::dispatch(
     bool err = false;
 
     amd_kernel_list list;
+
     if (param.normalize_variance) {
         AMD_API::mem_set(_sd.mutable_data(), 0, _mean.valid_size() * sizeof(OpDataType));
 
         if (_kernels_ptr[j] == NULL || _kernels_ptr[j].get() == NULL) {
-            ALOGE("Kernel is not exist");
+            LOG(ERROR) << "Kernel is not exist";
             return SaberInvalidValue;
         }
 
         err = _kernels_ptr[j].get()->SetKernelArgs(
-                (PtrDtype)inputs[0]->data(),
-                (int)num,
-                (int)inner_dim,
-                (PtrDtype)_mean.mutable_data(),
-                (PtrDtype)_sd.mutable_data());
+                  (PtrDtype)inputs[0]->data(),
+                  (int)num,
+                  (int)inner_dim,
+                  (PtrDtype)_mean.mutable_data(),
+                  (PtrDtype)_sd.mutable_data());
         list.push_back(_kernels_ptr[j]);
         j++;
+
         if (_kernels_ptr[j] == NULL || _kernels_ptr[j].get() == NULL) {
-            ALOGE("Kernel is not exist");
+            LOG(ERROR) << "Kernel is not exist";
             return SaberInvalidValue;
         }
+
         err = _kernels_ptr[j].get()->SetKernelArgs(
-                (PtrDtype)inputs[0]->data(),
-                (int)num,
-                (int)inner_dim,
-                (float)(1.0 / inner_dim),
-                (PtrDtype)_mean.mutable_data(),
-                (PtrDtype)_sd.mutable_data(),
-                (float)param.eps,
-                (PtrDtype)outputs[0]->mutable_data());
+                  (PtrDtype)inputs[0]->data(),
+                  (int)num,
+                  (int)inner_dim,
+                  (float)(1.0 / inner_dim),
+                  (PtrDtype)_mean.mutable_data(),
+                  (PtrDtype)_sd.mutable_data(),
+                  (float)param.eps,
+                  (PtrDtype)outputs[0]->mutable_data());
         list.push_back(_kernels_ptr[j]);
 
     } else {
         if (_kernels_ptr[j] == NULL || _kernels_ptr[j].get() == NULL) {
-            ALOGE("Kernel is not exist");
+            LOG(ERROR) << "Kernel is not exist";
             return SaberInvalidValue;
         }
+
         err = _kernels_ptr[j].get()->SetKernelArgs(
-                (PtrDtype)inputs[0]->data(),
-                (int)num,
-                (int)inner_dim,
-                (PtrDtype)_mean.mutable_data());
+                  (PtrDtype)inputs[0]->data(),
+                  (int)num,
+                  (int)inner_dim,
+                  (PtrDtype)_mean.mutable_data());
         list.push_back(_kernels_ptr[j]);
         j++;
+
         if (_kernels_ptr[j] == NULL || _kernels_ptr[j].get() == NULL) {
-            ALOGE("Kernel is not exist");
+            LOG(ERROR) << "Kernel is not exist";
             return SaberInvalidValue;
         }
+
         err = _kernels_ptr[j].get()->SetKernelArgs(
-                (PtrDtype)inputs[0]->data(),
-                (int)num,
-                (int)inner_dim,
-                (float)(1.0 / inner_dim),
-                (PtrDtype)_mean.mutable_data(),
-                (PtrDtype)outputs[0]->mutable_data());
+                  (PtrDtype)inputs[0]->data(),
+                  (int)num,
+                  (int)inner_dim,
+                  (float)(1.0 / inner_dim),
+                  (PtrDtype)_mean.mutable_data(),
+                  (PtrDtype)outputs[0]->mutable_data());
         list.push_back(_kernels_ptr[j]);
     }
 
     if (!err) {
-        ALOGE("Failed to set kernel args");
+        LOG(ERROR) << "Failed to set kernel args";
         return SaberInvalidValue;
     }
 
     err = LaunchKernel(cm, list);
 
     if (!err) {
-        ALOGE("Failed to set execution");
+        LOG(ERROR) << "Failed to set execution";
         return SaberInvalidValue;
     }
 
-    ALOGD("COMPLETE EXECUTION");
+    LOG_IF_S(INFO, ENABLE_AMD_DEBUG_LOG) << "COMPLETE EXECUTION";
     return SaberSuccess;
 }
 template class SaberMvn<AMD, AK_FLOAT>;
